9dbaf64bc7efb470305a235dd7a84fd68800a47c
mobilecrazy51-afk <mobilecrazy51@gmail.com>
2025-10-06 11:19:47 +0630

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "g12-study-v1";
+const urlsToCache = [
+  "index.html",
+  "exercise.html",
+  "video.html",
+  "policy.html",
+  "terms.html",
+  "assets/style.css",
+  "assets/logo.png"
+];
+
+// Install event
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
